cf10a1a9f98add894904d3df9d3cb7efee2d65d3
mr <mr@mr-PC>
2022-03-16 00:41:34 +0530

    change in print statement

--- Dummy Project/src/DummyPackage/Dummy_class.java
+++ Dummy Project/src/DummyPackage/Dummy_class.java
@@ -6,7 +6,7 @@ public class Dummy_class {
 		// TODO Auto-generated method stub
 		int a=12;
 		if(a%2==0) {
-		System.out.println(a+" is even number");
+		    System.out.println(a+" is even number");
 		}else {
 			System.out.println(a+" is odd number");
 		}
