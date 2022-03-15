fac3159ec2db8f3f90e1ba79083e48c3f99e5a02
mr <mr@mr-PC>
2022-03-16 00:47:35 +0530

    allignment change

--- Dummy Project/src/DummyPackage/Dummy_class.java
+++ Dummy Project/src/DummyPackage/Dummy_class.java
@@ -5,6 +5,7 @@ public class Dummy_class {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a=22;
+		
 		if(a%2==0) {
 		    System.out.println(a+" is even number");
 		}else {
