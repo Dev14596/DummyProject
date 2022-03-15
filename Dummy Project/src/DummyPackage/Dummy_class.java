c2286f9b1dc092812ddb8e918cc5762d90e7a1a9
Dev <Dev@DESKTOP-0C6QHMM>
2022-03-15 23:27:25 +0530

    test

---
+++ Dummy Project/src/DummyPackage/Dummy_class.java
@@ -0,0 +1,15 @@
+package DummyPackage;
+
+public class Dummy_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=12;
+		if(a%2==0) {
+		System.out.println("this is even no. : " +a);
+		}else {
+			System.out.println("this is a odd no. : "+a);
+		}
+	}
+
+}
