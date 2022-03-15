c7e1eea7706a8778a3e88059ddd00bcca286bb57
mr <mr@mr-PC>
2022-03-16 00:49:08 +0530

    value change

--- Dummy Project/src/DummyPackage/Dummy_class.java
+++ Dummy Project/src/DummyPackage/Dummy_class.java
@@ -4,7 +4,7 @@ public class Dummy_class {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a=22;
+		int a=242;
 		
 		if(a%2==0) {
 		    System.out.println(a+" is even number");
