05cad34395b092d4b089b8012a1a48290a9b18a2
sohan <sohan@VENOM>
2022-03-16 11:17:27 +0530

    change done

--- Dummy Project/src/DummyPackage/Dummy_class.java
+++ Dummy Project/src/DummyPackage/Dummy_class.java
@@ -3,8 +3,8 @@ package DummyPackage;
 public class Dummy_class {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int a=240002;
+		int a=10;
+		
 		
 		if(a%2==0) {
 		    System.out.println(a+" is even number");
