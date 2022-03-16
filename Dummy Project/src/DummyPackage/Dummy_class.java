bb0cce94352982168fff253ad14fd272b18399a3
mr <mr@mr-PC>
2022-03-16 11:10:20 +0530

    Value cahnge

--- Dummy Project/src/DummyPackage/Dummy_class.java
+++ Dummy Project/src/DummyPackage/Dummy_class.java
@@ -4,7 +4,7 @@ public class Dummy_class {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a=240002;
+		int a=2400024;
 		
 		if(a%2==0) {
 		    System.out.println(a+" is even number");
