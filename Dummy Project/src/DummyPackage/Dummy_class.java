371443f5c32ba7b0bd1a21033e526c9df1dcf025
sohan <sohan@VENOM>
2022-03-16 11:39:47 +0530

    abcd

--- Dummy Project/src/DummyPackage/Dummy_class.java
+++ Dummy Project/src/DummyPackage/Dummy_class.java
@@ -3,12 +3,12 @@ package DummyPackage;
 public class Dummy_class {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		int a=10;
-=======
+
 		// TODO Auto-generated method stub
-		int a=2400024;
->>>>>>> branch 'master' of https://github.com/Dev14596/DummyProject.git
+		int a1=2400024;
+
 		
 		System.out.println("dev");
 		
